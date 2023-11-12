c0d9451a73f60e53cfd02de7b97cecc1d8b9ecf9
AllyPham04 <phamngoclinh3122004@gmail.com>
2023-11-12 22:52:33 +0700

    Update account.py

--- account.py
+++ account.py
@@ -6,11 +6,19 @@ def app():
     if 'user_name' not in st.session_state:
         st.session_state['user_name'] = ' '
 
-    st.session_state['user_name'] = st.text_input(f'Name:', st.session_state['user_name'])
+    st.session_state['user_name'] = st.text_input("**Enter your name:**", st.session_state.get('user_name', ''))
     st.header(f"Welcome, {st.session_state['user_name']}!")
+
+
     total_balance = 0
     total_income = 0
     total_expense = 0
+
+    for income in user_income:
+        total_income += income["Amount"]
+    for expense in user_expense:
+        total_expense += expense["Amount"]
+    total_balance = total_income - total_expense
 
     col1, col2, col3 = st.columns(3)
     
@@ -27,15 +35,21 @@ def app():
         df = pd.DataFrame()
 
     if not df.empty:
-        
-        for date, transactions in df.groupby('Date'):
-            df = df.sort_values(by='Date', ascending=False)
+        df = df.sort_values(by='Date', ascending=False)
+        for date, transactions in df.groupby(df["Date"]):
             st.subheader(date)
             for _, transaction in transactions.iterrows():
-                col1, col2 = st.columns(2)
-            
                 amount = transaction["Amount"]
                 if transaction["Type"] == "Income":
-                    col1.write(f'{transaction["Category"]}: +{amount} {currency}')
+                    st.write(f'- {transaction["Category"]}: \+ {amount} {currency}')
                 elif transaction["Type"] == "Expense":
-                    col2.write(f'{transaction["Category"]}: -{amount} {currency}')
+                    st.write(f'- {transaction["Category"]}: \- {amount} {currency}')
+    
+    if st.button("Clear all data"):
+        st.session_state.clear()
+        if os.path.exists('data.csv'):
+            os.remove('data.csv')
+            st.success("Data cleared!")
+        col1.empty()
+        col2.empty()
+        col3.empty()
