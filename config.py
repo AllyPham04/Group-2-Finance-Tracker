43f6bd799ee1fa28706b286d3086b6bb77baf7df
AllyPham04 <phamngoclinh3122004@gmail.com>
2023-11-12 16:06:59 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+incomes = ["Salary", "Pocket Money", "Bonus", "Side Job", "Investment", "Other Income"]
+expenses = ["Food", "Clothes", "Houseware", "Cosmetic", "Exchange", "Education", "Electric Bill", "Transportation", "Other Expense"]
+currency = "VND"
+page_title = "Personal Finance Tracker"
+page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+layout = "centered"
+user_income = []
+user_expense = []
+transactions = []
