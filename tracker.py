7324449cdd6d1bb60f06ae687b67b65f22504792
AllyPham04 <phamngoclinh3122004@gmail.com>
2023-11-13 21:51:50 +0700

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -14,54 +14,73 @@ def app():
     total_expense = 0
 
     with col_b1:
-        with st.form("add_transaction", clear_on_submit=True):
-            st.subheader("Transaction")
-            type = st.radio("Type:",
-                            ["Income", "Expense"])
-            selected_date = st.date_input("Select date:", value=datetime.today(), format="DD/MM/YYYY")
-            amount = st.number_input(f"Amount:", min_value=0, format="%i", step=10, key="amount_input")
-            if type == "Income":
+        tab1, tab2 = st.tabs(["Income", "Expense"])
+        
+        with tab1:
+            with st.form("income", clear_on_submit=True):
+                st.subheader("Transaction")
+                selected_date = st.date_input("Select date:", value=datetime.today(), format="DD/MM/YYYY")
+                amount = st.number_input(f"Amount:", min_value=0, format="%i", step=10)
                 category = st.selectbox("Category:", incomes)
-            elif type == "Expense":
-                category = st.selectbox("Category:", expenses)
-            if st.form_submit_button("Save Data"):
-                # Gather user inputs
-                user_data = {
-                    'Type': 'Income' if type == "Income" else 'Expense',
-                    'Date': selected_date.strftime("%d-%m-%Y"),
-                    'Category': category,
-                    'Amount': amount
-                }
-                        
-                try:
-                    df = pd.read_csv('data.csv')
-                except (FileNotFoundError, pd.errors.EmptyDataError):
-                    df = pd.DataFrame(columns=user_data.keys())
-                        
-                df = pd.concat([df, pd.DataFrame(user_data, index=[0])], ignore_index=True)
-                df = df.sort_values(by=['Date'], ascending=False)
-                df.to_csv('data.csv', index=False, date_format='%H:%M:%S')
+                if st.form_submit_button("Save Data"):
+                    # Gather user inputs
+                    user_data = {
+                        'Type': 'Income',
+                        'Date': selected_date.strftime("%d-%m-%Y"),
+                        'Category': category,
+                        'Amount': amount
+                    }
+                                
+                    try:
+                        df = pd.read_csv('data.csv')
+                    except (FileNotFoundError, pd.errors.EmptyDataError):
+                        df = pd.DataFrame(columns=user_data.keys())
+                                
+                    df = pd.concat([df, pd.DataFrame(user_data, index=[0])], ignore_index=True)
+                    df = df.sort_values(by=['Date'], ascending=False)
+                    df.to_csv('data.csv', index=False, date_format='%H:%M:%S')
 
-                st.write(f"{category}: {amount} {currency}")
-                st.success("Data saved!")
+                    st.success("Data saved!")
+
+        with tab2:
+            with st.form("expense", clear_on_submit=True):
+                st.subheader("Transaction")
+                selected_date = st.date_input("Select date:", value=datetime.today(), format="DD/MM/YYYY")
+                amount = st.number_input(f"Amount:", min_value=0, format="%i", step=10)
+                category = st.selectbox("Category:", expenses)
+                if st.form_submit_button("Save Data"):
+                    # Gather user inputs
+                    user_data = {
+                        'Type': 'Expense',
+                        'Date': selected_date.strftime("%d-%m-%Y"),
+                        'Category': category,
+                        'Amount': amount
+                    }
+                                
+                    try:
+                        df = pd.read_csv('data.csv')
+                    except (FileNotFoundError, pd.errors.EmptyDataError):
+                        df = pd.DataFrame(columns=user_data.keys())
+                                
+                    df = pd.concat([df, pd.DataFrame(user_data, index=[0])], ignore_index=True)
+                    df = df.sort_values(by=['Date'], ascending=False)
+                    df.to_csv('data.csv', index=False, date_format='%H:%M:%S')
+
+                    st.success("Data saved!")
+            
                 
-    with col_a1:
-        if os.path.exists('data.csv'):
-            df = pd.read_csv('data.csv')
-            total_income = df[df['Type'] == 'Income']['Amount'].sum()
-            total_expense = df[df['Type'] == 'Expense']['Amount'].sum()
-            total_balance = total_income - total_expense
-        else:
-            total_balance = 0
-        st.metric("Total Credits", f"{total_balance} {currency}")
 
     with col_b2:
         with st.form("transactions_history", clear_on_submit=True):
             st.subheader("History")
             if os.path.exists('data.csv'):
                 df = pd.read_csv('data.csv')
+                total_income = df[df['Type'] == 'Income']['Amount'].sum()
+                total_expense = df[df['Type'] == 'Expense']['Amount'].sum()
+                total_balance = total_income - total_expense
             else:
                 df = pd.DataFrame()
+                total_balance = 0
 
             if not df.empty:
                 df.index = df.index + 1
@@ -77,3 +96,8 @@ def app():
                     st.success("Data cleared!")
                 user_income.clear()
                 user_expense.clear()
+    
+    with col_a1:
+        st.metric("Total Credits", f"{total_balance} {currency}")
+    
+    #with col_a2:
