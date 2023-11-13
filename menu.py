d5631a2e9c809609fc22317f6d02e2ec89acb426
AllyPham04 <phamngoclinh3122004@gmail.com>
2023-11-13 11:58:52 +0700

    Update menu.py

--- menu.py
+++ menu.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 def get_selected_option():
     return st.sidebar.radio(label="Menu", 
-                                options=["Account", "Set Budget", "Tracker", "Report"])
+                            options=["Account", "Set Budget", "Tracker", "Report"])
