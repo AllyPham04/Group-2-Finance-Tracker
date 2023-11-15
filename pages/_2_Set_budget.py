cfc62e7f58685a252289cef0fe0061ca89de3c75
bacxiudaxay <buianhbuianh25@gmail.com>
2023-11-15 14:51:21 +0700

    Update _2_Set_budget.py

--- pages/_2_Set_budget.py
+++ pages/_2_Set_budget.py
@@ -1,2 +1,14 @@
-pass
-#code ở đây
+from _3_Tracker import *
+
+import streamlit as st
+
+st.title("Set Budget ")
+
+budget = st.number_input("Set Your Buget :", min_value=0.0, step=100.0)
+
+if budget > 0:
+    st.write(f"your budget is : ${budget}")
+
+if total_expense > budget :
+    st.warning(f" over budget :${total_expense-budget} ")
+    
