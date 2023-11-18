00dc7a387fd014f5706a1b981d306c1931c39e70
AllyPham04 <phamngoclinh3122004@gmail.com>
2023-11-18 22:38:15 +0700

    Update _4_Report.py

--- pages/_4_Report.py
+++ pages/_4_Report.py
@@ -1,3 +1,4 @@
+import datetime as dt
 import os
 import pandas as pd
 import plotly.express as px
@@ -6,27 +7,43 @@ import pytz
 from config import *
 from datetime import datetime
 
-# -----------------------------------------
 st.set_page_config(layout=layout)
 st.title('Report')
+# -----------------------------------------
+ss = st.session_state
 # side_bar = st.sidebar
 try:
-    df = pd.read_csv('data.csv', parse_dates=['Date'], dayfirst=True)
+    file_path = 'data.csv'
+    df = pd.read_csv(file_path, parse_dates=['Date'], dayfirst=True)
 except (FileNotFoundError, pd.errors.EmptyDataError):
     df = pd.DataFrame(columns=['Type', 'Date', 'Category', 'Amount'])
 # -----------------------------------------
 
-range_col, summary_col = st.columns([1, 2], gap='medium')
-# range_col.title("COL 1")
-# summary_col.title("COL 2")
-
 now = datetime.now()
 now_vn = now.astimezone(pytz.timezone('Asia/Ho_Chi_Minh'))
 
-with range_col:
-    # range_col_manual_select, range_col_quick_select = st.tabs(['Manual Selection', 'Quick Selection'])
-    #
-    # with range_col_manual_select:
+def summarize(summary_df):
+# Set name which divided into 3 columns
+    summary_income, summary_expense, summary_saving = st.columns(3)
+
+    # Calculate each column
+    summary_sum = summary_df.groupby(['Type'])['Amount'].sum()
+    summary_sum_income = summary_sum.get('Income', 0)
+    summary_sum_expense = summary_sum.get('Expense', 0)
+    summary_sum_saving = summary_sum.get('Income', 0) - summary_sum.get('Expense', 0)
+
+    # Write each column
+    summary_income.subheader('Total Income')
+    summary_income.subheader(f"{summary_sum_income:,}")
+    summary_expense.subheader('Total Expense')
+    summary_expense.subheader(f"{summary_sum_expense:,}")
+    summary_saving.subheader('Balance')
+    summary_saving.subheader(f"{summary_sum_saving:,}")
+
+
+range_col_manual_select, range_col_quick_select = st.tabs(['Manual Selection', 'Quick Selection'])
+with range_col_manual_select:
+
     with st.form('range_form', clear_on_submit=False):
 
         range_manual_start_date_input, range_manual_end_date_input = st.columns(2)
@@ -45,66 +62,28 @@ with range_col:
                                   options=['Income and Expense', 'Categories'])
 
         range_button = st.form_submit_button('Submit')
-    # with range_col_quick_select:
-    #     quick_select = st.selectbox('By',['Week', 'Month', 'Year'])
-    #     weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    #     today = dt.date.today()
-    #     range_quick_start_date = today - dt.timedelta(days=today.weekday())
-    #     range_quick_end_date = today + dt.timedelta(days=6 - today.weekday())
-    #
-    #     minus_var = st.session_state.get('minus_var', 0)
-    #     test_minus_button = st.button('Click here to go to previous week')
-    #     if test_minus_button:
-    #         minus_var += 1
-    #         st.session_state['minus_var'] = minus_var
-    #
-    #
-    #     range_quick_start_date = range_quick_start_date - dt.timedelta(days=7 * minus_var)
-    #     range_quick_end_date = range_quick_end_date - dt.timedelta(days=7 * minus_var)
-    #     st.write(range_quick_start_date, range_quick_end_date)
-
-with summary_col:
-    # Set name which divided into 3 columns
-    summary_income, summary_expense, summary_saving = st.columns(3)
-
-    # Calculate each column
-    summary_sum = range_df.groupby(['Type'])['Amount'].sum()
-    summary_sum_income = summary_sum.get('Income', 0)
-    summary_sum_expense = summary_sum.get('Expense', 0)
-    summary_sum_saving = summary_sum.get('Income', 0) - summary_sum.get('Expense', 0)
-
-    # Write each column
-    summary_income.subheader('Total Income')
-    summary_income.metric('Income', f"{summary_sum_income} {currency}")
-    summary_expense.subheader('Total Expense')
-    summary_expense.metric('Expense', f"{summary_sum_expense} {currency}")
-    summary_saving.subheader('Total Balance')
-    summary_saving.metric('Balance', f"{summary_sum_saving} {currency}")
-
-if not df.empty:
-    visualize_type, temp = st.columns([1, 8])
-    if range_type == 'Income and Expense':
-        visual_df = df.groupby(['Date', 'Type'])['Amount'].sum()
-        visual_df = visual_df.reset_index()
-        visualize_type = visualize_type.selectbox('Visualization', 
-                                                ['Line chart', 'Bar chart'])
-    elif range_type == 'Categories':
-        visual_df = range_df
-        # visual_df = visual_df.reset_index()
-        # ---------------------------------------------------
-        # De cai o visualization no ngan di
-        visualize_opt, tmp = st.columns([1, 3])
-
-        with visualize_opt:
+    options, summary = st.columns(2, gap='large')
+    with options:
+        if range_type == 'Income and Expense':
+            visualize_type, tmp = st.columns(2)
+            visual_df = range_df.groupby(['Date', 'Type'])['Amount'].sum()
+            visual_df = visual_df.reset_index()
+            visualize_type = visualize_type.selectbox('Visualization', ['Line chart', 'Bar chart'])
+        elif range_type == 'Categories':
+            visual_df = range_df
+            # visual_df = visual_df.reset_index()
+            # ---------------------------------------------------
+            # De cai o visualization no ngan di
             with st.form('visualize_categories', clear_on_submit=False):
                 visualize_type, visualize_cate_type = st.columns(2)
-                visualize_type = visualize_type.selectbox('Visualization type', 
-                                                        ['Line chart', 'Bar chart', 'Pie chart'])
-                visualize_cate_type = visualize_cate_type.selectbox('Type', 
-                                                                    ['Income', 'Expense'])
+                visualize_type = visualize_type.selectbox('Visualization type', ['Line chart', 'Bar chart', 'Pie chart'])
+                visualize_cate_type = visualize_cate_type.selectbox('Type', ['Income', 'Expense'])
                 visualize_submit = st.form_submit_button('Submit')
+    with summary:
+        summarize(range_df)
 
-    visualization, ranking_income, ranking_expense = st.columns([2, 1, 1], gap='medium')
+
+    visualization = st.container()
     with visualization:
         if range_type == 'Income and Expense':
             if visualize_type == 'Line chart':
@@ -199,14 +178,103 @@ if not df.empty:
                     )
                     st.plotly_chart(visual, use_container_width=True)
 
-    with ranking_income:
-        ranking_income.header('Income')
-        rank_income_df = df[df['Type'] == 'Income'].copy()
-        rank_income_df = rank_income_df.groupby(['Category'])['Amount'].sum()
-        st.dataframe(rank_income_df.head(5), use_container_width=True)
+with range_col_quick_select:
+    quick_select, tmp = st.columns([1,4])
+    quick_select = quick_select.selectbox('By',['Week', 'Month', 'Year'])
 
-    with ranking_expense:
-        ranking_expense.header('Expense')
-        rank_expense_df = df[df['Type'] == 'Expense'].copy()
-        rank_expense_df = rank_expense_df.groupby(['Category'])['Amount'].sum()
-        st.dataframe(rank_expense_df.head(5), use_container_width=True)
+    weekdays = ['Monday', 'Monday', 'Tuesday', 'Tuesday', 'Wednesday', 'Wednesday', 'Thursday',
+                'Thursday', 'Friday', 'Friday', 'Saturday', 'Saturday', 'Sunday', 'Sunday']
+
+    today = dt.date.today()
+
+    range_quick_start_date = today - dt.timedelta(days=today.weekday())
+    range_quick_end_date = today + dt.timedelta(days=6 - today.weekday())
+    range_type, tmp = st.columns([1,4])
+    range_type = range_type.selectbox(label='Type', options=['Income and Expense'])
+    quick_sub_button, quick_add_button, quick_date_text, tmp = st.columns([.05, .05, .2, .6], gap='medium')
+
+    if 'quick_change_var' not in ss:
+        ss.quick_change_var = 0
+
+    def quick_sub_def():
+        ss.quick_change_var -= 1
+
+    def quick_add_def():
+        ss.quick_change_var += 1
+    quick_sub_button = quick_sub_button.button(':arrow_backward:', key='range_sub_button', on_click=quick_sub_def)
+    quick_add_button = quick_add_button.button(':arrow_forward:', key='range_add_button', on_click=quick_add_def)
+
+    range_quick_start_date = range_quick_start_date + dt.timedelta(days=7 * ss.quick_change_var)
+    range_quick_end_date = range_quick_end_date + dt.timedelta(days=7 * ss.quick_change_var)
+    date_text = f'<div style="border: 2px solid #A0A0A0; font-size: 20px; text-align: center;"> From {range_quick_start_date} to {range_quick_end_date}</div>'
+    quick_date_text = quick_date_text.markdown(date_text, unsafe_allow_html=True)
+
+    range_quick_start_date = pd.to_datetime(range_quick_start_date, format='%d-%m-%Y')
+    range_quick_end_date = pd.to_datetime(range_quick_end_date, format='%d-%m-%Y')
+
+    range_quick_df = df.copy()
+    range_quick_df['Date'] = pd.to_datetime(range_quick_df['Date'])
+    range_condition = (range_quick_df['Date'] >= range_quick_start_date) & (range_quick_df['Date'] <= range_quick_end_date)
+    range_quick_df = range_quick_df[range_condition]
+
+    # Select chart
+    options, summary = st.columns(2)
+    with options:
+        if range_type == 'Income and Expense':
+            visual_df = range_quick_df.groupby(['Date', 'Type'])['Amount'].sum().reset_index()
+            date_range = pd.date_range(start=range_quick_start_date, end=range_quick_end_date, freq='D')
+            date_type_combinations = pd.MultiIndex.from_product([date_range, df['Type'].unique()], names=['Date', 'Type'])
+
+            # Reindex the DataFrame with the date and type combinations
+            visual_df = (
+                visual_df.set_index(['Date', 'Type'])
+                .reindex(date_type_combinations, fill_value=0)
+                .reset_index()
+            )
+            visual_df = visual_df[(visual_df['Type'] != 0) | (visual_df['Amount'] != 0)]
+            visualize_type, tmp = st.columns(2)
+            visualize_type = visualize_type.selectbox('Charts', ['Line chart', 'Bar chart'])
+
+    with summary:
+        summarize(range_quick_df)
+
+    visualization = st.container()
+    with visualization:
+        if range_type == 'Income and Expense':
+            if visualize_type == 'Line chart':
+
+                visual = px.line(
+                    visual_df,
+                    x=weekdays,
+                    y='Amount',
+                    color='Type'
+                )
+                st.plotly_chart(visual, use_container_width=True)
+            #
+            elif visualize_type == 'Bar chart':
+
+                visual = px.bar(
+                    visual_df,
+                    x=weekdays,
+                    y='Amount',
+                    color='Type',
+                    barmode="stack",
+                )
+                st.plotly_chart(visual, use_container_width=True)
+
+             # ------------------------------------------------------
+rank_income, rank_expense, tmp = st.columns([1,1,2])
+
+with rank_income:
+    st.header('Top Income')
+    rank_income_df = df.copy()
+    rank_income_df = rank_income_df.groupby('Category')['Amount'].sum()
+    # rank_income_df = rank_income_df.sort_values('Amount', ascending=False)
+    st.dataframe(rank_income_df)
+
+with rank_expense:
+    st.header('Top Expense')
+    rank_expense_df = df.copy()
+    rank_expense_df = rank_expense_df.groupby('Category')['Amount'].sum()
+    # rank_expense_df = rank_expense_df.sort_values('Amount', ascending=False)
+    st.dataframe(rank_expense_df)
