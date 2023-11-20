221c48c10e8330f8ea9ff0049339479f1e9985ff
trộn <78654130+trong1234ar@users.noreply.github.com>
2023-11-20 10:44:23 +0700

    Add files via upload
    add visualize

--- Tracker.py
+++ Tracker.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import os
-import calendar
 import pandas as pd
 import plotly.express as px
 import pytz
@@ -11,16 +10,12 @@ from millify import millify
 
 def track():
     st.title("Tracker")
-    col_a1, col_a2, col_a3 = st.columns([0.2, 0.4, 0.4])
+    col_a1, col_a2, col_a3 = st.columns([1, 1, 2])
     #st.divider()
     col_b1, col_b2 = st.columns(2)
 
     now = datetime.now()
     now_vn = now.astimezone(pytz.timezone('Asia/Ho_Chi_Minh'))
-
-    _, last_day = calendar.monthrange(now_vn.year, now_vn.month)
-    first_day_of_month = datetime(now_vn.year, now_vn.month, 1).date()
-    last_day_of_month = datetime(now_vn.year, now_vn.month, last_day).date()
 
     total_income = 0
     total_expense = 0
@@ -32,7 +27,7 @@ def track():
 
     with col_b1:
         tab1, tab2 = st.tabs(["Income", "Expense"])
-            
+
         with tab1:
             with st.form("income", clear_on_submit=True):
                 st.subheader("Transaction")
@@ -47,20 +42,20 @@ def track():
                         'Category': category,
                         'Amount': amount
                     }
-                                    
+
                     try:
                         history_df = pd.read_csv('data.csv')
                         #budget_df = pd.read_csv('budget.csv')
                     except (FileNotFoundError, pd.errors.EmptyDataError):
                         history_df = pd.DataFrame(columns=user_data.keys())
                         #budget_df = pd.DataFrame(columns=['Type', 'Category', 'Budget'])
-                                    
+
                     history_df = pd.concat([pd.DataFrame(user_data, index=[0]), history_df], ignore_index=True)
                     history_df['Date'] = pd.to_datetime(history_df['Date'], format="%d-%m-%Y")
                     history_df = history_df.sort_values(by=['Date'], ascending=False)
                     history_df.to_csv('data.csv', index=False, date_format="%d-%m-%Y")
 
-                    st.success("Data saved!")                
+                    st.success("Data saved!")
 
         with tab2:
             with st.form("expense", clear_on_submit=True):
@@ -76,12 +71,12 @@ def track():
                         'Category': category,
                         'Amount': amount
                     }
-                                    
+
                     try:
                         history_df = pd.read_csv('data.csv')
                     except (FileNotFoundError, pd.errors.EmptyDataError):
                         history_df = pd.DataFrame(columns=user_data.keys())
-                                
+
                     history_df = pd.concat([pd.DataFrame(user_data, index=[0]), history_df], ignore_index=True)
                     history_df['Date'] = pd.to_datetime(history_df['Date'], format="%d-%m-%Y")
                     history_df = history_df.sort_values(by=['Date'], ascending=False)
@@ -107,26 +102,30 @@ def track():
                 total_saving = 0
 
             if not history_df.empty:
-                monthly_df = history_df.copy() #monthly_data
-                monthly_df['Date'] = pd.to_datetime(monthly_df['Date'], dayfirst=True).dt.date
-                monthly_df = monthly_df[(monthly_df['Date'] >= first_day_of_month) & (monthly_df['Date'] <= last_day_of_month)]
+                copyhistory_df = history_df.copy()
+                #------------
+                col3_df = history_df.copy()
+                col3_df['Amount'] = history_df.apply(lambda row: row['Amount']
+                if row['Type'] == 'Income'
+                else row['Amount'] * -1, axis=1)
+                #------------
                 history_df.index = history_df.index + 1
-                history_df['Amount'] = history_df.apply(lambda row: f'+ {currency} {row["Amount"]}' 
-                                        if row['Type'] == 'Income' 
-                                        else f'- {currency} {row["Amount"]}', axis=1)
+                history_df['Amount'] = history_df.apply(lambda row: f'+ {row["Amount"]} {currency}'
+                if row['Type'] == 'Income'
+                else f'- {row["Amount"]} {currency}', axis=1)
                 #history_df['Month'] = history_df['Date'].dt.strftime("%m")
-                st.dataframe(history_df.drop(columns='Type'), use_container_width=True)
-            
-            
+                history_container = st.dataframe(history_df.drop(columns='Type'), use_container_width=True)
+
+
             if os.path.exists('budget.csv') and os.path.exists('data.csv'):
                 budget_df = pd.read_csv('budget.csv')
                 for expense in expenses:
                     if expense in budget_df['Category'].values:
                         budget_expense = float(budget_df[budget_df['Category'] == expense]['Budget'].values[0])
-                        expense_cate = float(monthly_df[(monthly_df['Type'] == 'Expense') & (monthly_df['Category'] == expense)]['Amount'].sum())
+                        expense_cate = float(copyhistory_df[(copyhistory_df['Type'] == 'Expense') & (copyhistory_df['Category'] == expense)]['Amount'].sum())
                         if expense_cate > 0.9 * budget_expense:
                             if not st.session_state.get(f'warning_{expense}', False):
-                                st.warning(f"You have spent over 90% of your budget for {expense} category in {calendar.month_name[now_vn.month]}")
+                                st.warning(f"You have spent over 90% of your budget for {expense} category")
                                 st.session_state[f'warning_{expense}'] = True
             else:
                 pass
@@ -146,26 +145,54 @@ def track():
     with col_a1:
         total_balance_millified = millify(total_balance, precision=2)
         with st.container():
-            st.subheader("Total Credits") 
-            st.metric('Balance', f"{currency} {total_balance_millified}", delta=f"{currency} {delta_balance_millified}", delta_color="normal")
-        
+            st.subheader("Total Credits")
+            st.metric('Balance', f"{total_balance_millified} {currency}", delta=f"{delta_balance_millified} {currency}", delta_color="normal")
+
     with col_a2:
         col_a2_1, col_a2_2 = st.columns(2)
         col_a2_1.subheader("Saving Goal")
 
         saving_goal = col_a2_1.number_input("Enter your saving goal:", min_value=0, format="%i", step=10)
 
+        progress = total_saving / saving_goal if saving_goal > 0 else 0
+
         if col_a2_1.button("Save"):
-            col_a2_1.write(f'Your saving goal is {currency} {saving_goal}')
+            col_a2_1.write(f'Your saving goal is {saving_goal} {currency}')
             if total_saving >= saving_goal:
                 st.success("Congratulations! You have reached your saving goal!")
             elif total_saving == 0:
                 st.warning("You have not saved anything yet!")
             else:
-                fig_saving = px.pie(values=[total_saving, saving_goal - total_saving], 
-                                names=["Saving", "Remaning"], 
-                                title=f'Saving Progress')
+                fig_saving = px.pie(values=[total_saving, saving_goal - total_saving],
+                                    names=["Saving", "Remaning"],
+                                    title=f'Saving Progress')
                 col_a2_2.plotly_chart(fig_saving, use_container_width=True)
-    
+
     with col_a3:
-        pass
+        st.subheader(f'Your balance in {datetime.now().year}')
+        visual_df = col3_df.copy()
+        visual_df['Date'] = pd.to_datetime(visual_df['Date'], format='%d-%m-%Y')
+        visual_df['Month'] = visual_df['Date'].dt.strftime('%b')
+
+        # Group by month and sum 'Amount' for each group
+        grouped_data = visual_df.groupby('Month')['Amount'].sum().reset_index()
+
+        # Create a DataFrame with all months in the desired range
+        all_months = pd.date_range(start=f'{datetime.now().year}-01-01', end=f'{datetime.now().year}-12-31', freq='M')
+        all_months_df = pd.DataFrame({'Date': all_months, 'Amount': 0})
+
+        # Convert the 'Date' column to month names in all_months_df
+        all_months_df['Month'] = all_months_df['Date'].dt.strftime('%b')
+
+        # Merge the two DataFrames, filling missing values with 0
+        result_df = pd.merge(all_months_df, grouped_data, on='Month', how='left').fillna(0)
+
+        visual = px.line(
+            result_df,
+            x='Month',
+            y='Amount_y',
+            markers=True,
+        )
+        visual.update_layout(height=300, xaxis_title='Month', yaxis_title='Total Amount')
+        st.plotly_chart(visual, use_container_width=True)
+
