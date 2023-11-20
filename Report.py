53007c7c99cff160fb82069d3a130e8a84df6566
trộn <78654130+trong1234ar@users.noreply.github.com>
2023-11-21 03:28:12 +0700

    update tat ca cac file va them image
    1. final: sửa màu sidebar
    2. config: thêm:
    -library: base 64
    - fontFamily = 'Candara' , app_name = 'KEEPEE'
    -functions: get_img_as_base64, local_css, background_about_us, background
    3. các file còn lại: add func: local_css, background từ config

--- Report.py
+++ Report.py
@@ -3,11 +3,14 @@ import os
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+import pytz
 from config import *
 from datetime import datetime
 
 
 def rep():
+    local_css('style.css')
+    background()
     st.title('Report')
     # -----------------------------------------
     ss = st.session_state
@@ -20,6 +23,7 @@ def rep():
     # -----------------------------------------
 
     now = datetime.now()
+    now_vn = now.astimezone(pytz.timezone('Asia/Ho_Chi_Minh'))
 
     def summarize(summary_df):
     # Set name which divided into 3 columns
@@ -33,11 +37,11 @@ def rep():
 
         # Write each column
         summary_income.subheader('Total Income')
-        summary_income.subheader(f"{currency} {summary_sum_income:,}")
+        summary_income.subheader(f"{summary_sum_income:,}")
         summary_expense.subheader('Total Expense')
-        summary_expense.subheader(f"{currency} {summary_sum_expense:,}")
+        summary_expense.subheader(f"{summary_sum_expense:,}")
         summary_saving.subheader('Balance')
-        summary_saving.subheader(f"{currency} {summary_sum_saving:,}")
+        summary_saving.subheader(f"{summary_sum_saving:,}")
 
 
     range_col_manual_select, range_col_quick_select = st.tabs(['Manual Selection', 'Quick Selection'])
@@ -46,8 +50,8 @@ def rep():
         with st.form('range_form', clear_on_submit=False):
 
             range_manual_start_date_input, range_manual_end_date_input = st.columns(2)
-            range_manual_start_date_input = range_manual_start_date_input.date_input('From', value=now.date(), format="DD/MM/YYYY")
-            range_manual_end_date_input = range_manual_end_date_input.date_input('To', value=now.date(), format="DD/MM/YYYY")
+            range_manual_start_date_input = range_manual_start_date_input.date_input('From', value=now_vn.date(), format="DD/MM/YYYY")
+            range_manual_end_date_input = range_manual_end_date_input.date_input('To', value=now_vn.date(), format="DD/MM/YYYY")
             range_manual_start_date = pd.to_datetime(
                 range_manual_start_date_input, format='%Y-%m-%d')
             range_manual_end_date = pd.to_datetime(
@@ -60,7 +64,7 @@ def rep():
             range_type = st.selectbox(label='Type', 
                                     options=['Income and Expense', 'Categories'])
 
-            st.form_submit_button('Submit')
+            range_button = st.form_submit_button('Submit')
         options, summary = st.columns(2, gap='large')
         with options:
             if range_type == 'Income and Expense':
@@ -77,7 +81,7 @@ def rep():
                     visualize_type, visualize_cate_type = st.columns(2)
                     visualize_type = visualize_type.selectbox('Visualization type', ['Line chart', 'Bar chart', 'Pie chart'])
                     visualize_cate_type = visualize_cate_type.selectbox('Type', ['Income', 'Expense'])
-                    st.form_submit_button('Submit')
+                    visualize_submit = st.form_submit_button('Submit')
 
         with summary:
             summarize(range_df)
@@ -178,7 +182,7 @@ def rep():
 
     with range_col_quick_select:
         quick_select, tmp = st.columns([1,4])
-        quick_select = quick_select.selectbox('By',['Week'])
+        quick_select = quick_select.selectbox('By',['Week', 'Month', 'Year'])
 
         # weekdays = ['Monday', 'Monday', 'Tuesday', 'Tuesday', 'Wednesday', 'Wednesday', 'Thursday',
         #             'Thursday', 'Friday', 'Friday', 'Saturday', 'Saturday', 'Sunday', 'Sunday']
@@ -263,35 +267,18 @@ def rep():
                     st.plotly_chart(visual, use_container_width=True)
 
                 # ------------------------------------------------------
-    with range_col_manual_select:
-        rank_income, rank_expense, tmp = st.columns([1,1,2])
-        with rank_income:
-            st.header('Top Income')
-            rank_income_df = range_df[range_df['Type'] == 'Income'].copy()
-            rank_income_df = rank_income_df.groupby(['Category'])['Amount'].sum()
-            # rank_income_df = rank_income_df.sort_values('Amount', ascending=False)
-            st.dataframe(rank_income_df)
+    rank_income, rank_expense, tmp = st.columns([1,1,2])
 
-        with rank_expense:
-            st.header('Top Expense')
-            rank_expense_df = range_df[range_df['Type'] == 'Expense'].copy()
-            rank_expense_df = rank_expense_df.groupby('Category')['Amount'].sum()
-            # rank_expense_df = rank_expense_df.sort_values('Amount', ascending=False)
-            st.dataframe(rank_expense_df)
-    
-    with range_col_quick_select:
-        quick = st.columns([1, 1, 2])
-        with quick[0]:
-            st.header('Top Income')
-            top_income_df = range_quick_df[range_quick_df['Type'] == 'Income'].copy()
-            top_income_df = top_income_df.groupby(['Category'])['Amount'].sum()
+    with rank_income:
+        st.header('Top Income')
+        rank_income_df = df[df['Type'] == 'Income'].copy()
+        rank_income_df = rank_income_df.groupby(['Category'])['Amount'].sum()
+        # rank_income_df = rank_income_df.sort_values('Amount', ascending=False)
+        st.dataframe(rank_income_df)
 
-            st.dataframe(top_income_df)
-        
-        with quick[1]:
-            st.header('Top Expense')
-            top_expense_df = range_quick_df[range_quick_df['Type'] == 'Expense'].copy()
-            top_expense_df = top_expense_df.groupby('Category')['Amount'].sum()
-
-            st.dataframe(top_expense_df)
-        
+    with rank_expense:
+        st.header('Top Expense')
+        rank_expense_df = df[df['Type'] == 'Expense'].copy()
+        rank_expense_df = rank_expense_df.groupby('Category')['Amount'].sum()
+        # rank_expense_df = rank_expense_df.sort_values('Amount', ascending=False)
+        st.dataframe(rank_expense_df)
--- config.py
+++ config.py
@@ -1,9 +1,53 @@
-incomes = ["Salary", "Saving", "Bonus", "Side Job", "Other Income"]
-expenses = ["Food", "Housing", "Entertainment", "Investment", "Education", "Utilities", "Transportation", "Other Expense"]
-currency = "$"
+import streamlit as st
+import  base64
+
+incomes = ["Salary", "Saving", "Bonus", "Side Job", "Investment", "Other Income"]
+expenses = ["Food", "Housing", "Entertainment", "Education", "Utilities", "Transportation", "Other Expense"]
+currency = "VND"
 page_title = "Personal Finance Tracker"
 page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 layout = "wide"
+fontFamily = 'Candara'
+app_name = 'KEEPEE'
 user_income = []
 user_expense = []
 transactions = []
+
+def get_img_as_base64(file):
+    with open(file, "rb") as f:
+        data = f.read()
+        return base64.b64encode(data).decode()
+
+def local_css(file_name):#func to read css file -> create flake
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+def background_about_us():
+    bg_img = get_img_as_base64("bg_2.png")
+    page_bg_img = f"""
+    <style>
+    [data-testid="stAppViewContainer"] > .main {{
+    background-image: url("data:image/png;base64,{bg_img}");
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-attachment: local;
+    }}
+    </style>
+    """
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+
+def background():
+    bg_img = get_img_as_base64("bg_3.png")
+    page_bg_img = f"""
+    <style>
+    [data-testid="stAppViewContainer"] > .main {{
+    background-image: url("data:image/png;base64,{bg_img}");
+    background-size: 100%;
+    background-repeat: no-repeat;
+    background-attachment: local;
+    }}
+    </style>
+    """
+
+    st.markdown(page_bg_img, unsafe_allow_html=True)
