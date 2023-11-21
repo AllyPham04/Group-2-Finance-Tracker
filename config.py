a8670729df5dc0c0aed287e12b38aecc0f855bac
AllyPham04 <phamngoclinh3122004@gmail.com>
2023-11-21 09:02:02 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import  base64
 
 incomes = ["Salary", "Saving", "Bonus", "Side Job", "Investment", "Other Income"]
 expenses = ["Food", "Housing", "Entertainment", "Education", "Utilities", "Transportation", "Other Expense"]
-currency = "$"
+currency = "VND"
 page_title = "Personal Finance Tracker"
 page_icon = ":money_with_wings:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 layout = "wide"
